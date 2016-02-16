bd944e38caa576d407a7f3e16e86598c507bf1f0
Rob Moore <rob.moore@gmail.com>
2016-02-16 10:57:31 -0600

    Adding Spring Actuator so that we have other endpoints to protect. Splitting up web app config so that greeting resource uses basic auth and other endpoints use form auth.

--- src/main/java/hello/SpringSecurityWebAppConfig.java
+++ src/main/java/hello/SpringSecurityWebAppConfig.java
@@ -1,20 +1,52 @@
 package hello;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpath;
 
-@Configuration
-public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.apply(stormpath()).and()
-                .antMatcher("/**")
-                .authorizeRequests()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+public class SpringSecurityWebAppConfig {
+
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http.apply(stormpath())
+                    .and()
+                    .antMatcher("/greeting")
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .httpBasic();
+        }
+    }
+
+    @Configuration
+    @ConfigurationProperties(prefix="hello")
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+        // Note: Add hello.admin-group href to application.yaml.
+        private String adminGroup;
+
+        public String getAdminGroup() {
+            return adminGroup;
+        }
+
+        public void setAdminGroup(String adminGroup) {
+            this.adminGroup = adminGroup;
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.apply(stormpath())
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().hasRole(adminGroup);
+        }
     }
 }
